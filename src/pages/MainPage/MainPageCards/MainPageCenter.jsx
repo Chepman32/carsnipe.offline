3b163a43cabacd4e650d3d057d8969471a7e89b1
Chepman32 <antonkerch555@gmail.com>
2025-02-05 10:46:09 +0300

    MainPageCenter

--- src/pages/MainPage/MainPageCards/MainPageCenter.jsx
+++ src/pages/MainPage/MainPageCards/MainPageCenter.jsx
@@ -1,23 +1,54 @@
-import { Typography } from 'antd';
-import React from 'react';
+import { Typography, Tooltip, Modal } from 'antd';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import "../mainPage.css"
 
 const MainPageCenter = ({ focused, handleMouseEnter, onClick, isMenuOpen }) => {
-
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const navigate = useNavigate();
-
   const { darkMode } = useSelector((state) => state.quickSettings);
 
   const handleClick = () => {
-    !isMenuOpen && navigate("/auctionsHub");
+    if (navigator.onLine) {
+      !isMenuOpen && navigate("/auctionsHub");
+    } else {
+      setIsModalVisible(true);
+    }
   };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
   return (
-    <div className={`tile ${darkMode ? 'darkTile' : ''} ${focused ? 'focused' : ''}`} onMouseEnter={() => handleMouseEnter("center")} onClick={handleClick}>
-      <Typography.Text className="mainpage__cardText_black">
-        Auctions
-      </Typography.Text>
-    </div>
+    <>
+      <Tooltip title={!navigator.onLine ? "You are offline" : ""} disabled={navigator.onLine}>
+        <div className={`tile ${darkMode ? 'darkTile' : ''} ${focused ? 'focused' : ''}`} onMouseEnter={() => handleMouseEnter("center")} onClick={handleClick} tabIndex={0} onKeyPress={(e) => { if (e.key === 'Enter') handleClick(); }}>
+          <Typography.Text className="mainpage__cardText_black">
+            Auctions
+          </Typography.Text>
+        </div>
+      </Tooltip>
+      <Modal
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={null}
+        closable={false}
+        className='offline_modal'
+      >
+        <h2>You are currently offline. Please check your internet connection.</h2>
+        <img src={require("../../../assets/images/offline-warning.png")} alt="Offline" style={{ width: '30rem', height: '30rem' }} />
+        <div className="offline_modal_okBtn" onClick={handleCancel}>
+          <p>ok</p>
+        </div>
+      </Modal>
+    </>
   );
 };
 
